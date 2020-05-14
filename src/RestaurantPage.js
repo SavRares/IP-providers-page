555bd92d71047e74ff1e2f8e8f1cd1fcfe13ccc7
SavRares <43829110+SavRares@users.noreply.github.com>
2020-05-14 18:40:52 +0300

    RestaurantPage component, API call fixed, needs slight rework at reviews and the colour feel to match the other pages.

--- src/RestaurantPage.js
+++ src/RestaurantPage.js
@@ -58,19 +58,16 @@ export class RestaurantPage extends Component {
   constructor(props){
     super(props);
     this.state = {
-      providerId: props.providerId
+      providerId: props.providerId,
+      isLoading: true,
+      courses: [],
+      providerName: null,
+      providerImages: [],
+      schedule: [],
+      description: null,
+      rating: 0,
+      specials: []
    }
-  }
-
-  vars = {
-    isLoading: true,
-    courses: [],
-    providerName: null,
-    providerImages: [],
-    schedule: [],
-    description: null,
-    rating: 0,
-    specials: []
   }
 
 
@@ -92,8 +89,8 @@ export class RestaurantPage extends Component {
   }
 
     render(){
-      if (this.vars.isLoading) {
-        return(<p>Loading...{this.state.providerId}</p>)
+      if (this.state.isLoading) {
+        return(<p>Loading...</p>)
       } else {
 
         return (
@@ -106,11 +103,14 @@ export class RestaurantPage extends Component {
               <div className='page'>
                 <BackButton />
                 <div id="wrapper">
-                <Content providerName={this.vars.providerName} providerImages={this.vars.providerImages} />
+                <Content providerName={this.state.providerName} providerImages={this.state.providerImages} />
                   <Row xl={2} lg={2} md={2} sm={2} xs={1}>
+                  <Col xl={{span: 8}} lg={{span: 8}} md={{span: 8}} sm={{span: 8}}>
+                      <Menu courses={this.state.courses}/>
+                    </Col>
                     <Col xl={{span: 4}} lg={{span: 4}} md={{span: 4}} sm={{span: 4}}>
-                      <Information description={this.vars.description} rating={this.vars.rating} specials={this.vars.specials} />
-                      <Schedule schedule={this.vars.schedule} />
+                      <Information description={this.state.description} rating={this.state.rating} specials={this.state.specials} />
+                      <Schedule schedule={this.state.schedule} />
                       <div id="reviewsdiv">
                           <Review id={1} userID={1} userPicture={profileTemp} username={"George Bright"} score={5} content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rutrum blandit condimentum. Morbi sodales libero cursus, bibendum nisl at, feugiat quam. Curabitur molestie sed dui luctus facilisis. Phasellus et enim a libero interdum sagittis eu eget justo. Duis suscipit tincidunt aliquam. Nulla nec gravida magna. Vestibulum laoreet diam ut tortor pretium ullamcorper. Integer sed felis quis justo imperdiet maximus eget ut mi. Pellentesque placerat facilisis nisi eu laoreet. Suspendisse commodo tempor risus, vitae feugiat enim malesuada non. Aenean lacinia odio massa, quis scelerisque lectus rhoncus volutpat. Nullam quis rhoncus dui. Donec lacinia sem porta augue tempus semper. Quisque porta egestas mauris, sed pretium nunc. Aliquam et eros porta, elementum diam ut, vulputate mauris. Pellentesque id eleifend lorem, eget blandit mi."}/>
                           <Review id={2} userID={3} userPicture={profileTemp} username={"John Hunter"} score={1} content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rutrum blandit condimentum. Morbi sodales libero cursus, bibendum nisl at, feugiat quam. Curabitur molestie sed dui luctus facilisis. Phasellus et enim a libero interdum sagittis eu eget justo. Duis suscipit tincidunt aliquam. Nulla nec gravida magna. Vestibulum laoreet diam ut tortor pretium ullamcorper. Integer sed felis quis justo imperdiet maximus eget ut mi. Pellentesque placerat facilisis nisi eu laoreet. Suspendisse commodo tempor risus, vitae feugiat enim malesuada non. Aenean lacinia odio massa, quis scelerisque lectus rhoncus volutpat. Nullam quis rhoncus dui. Donec lacinia sem porta augue tempus semper. Quisque porta egestas mauris, sed pretium nunc. Aliquam et eros porta, elementum diam ut, vulputate mauris. Pellentesque id eleifend lorem, eget blandit mi."}/>
@@ -121,9 +121,6 @@ export class RestaurantPage extends Component {
                           <Review id={7} userID={43} userPicture={profileTemp} username={"Default"} score={3} content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rutrum blandit condimentum. Morbi sodales libero cursus, bibendum nisl at, feugiat quam. Curabitur molestie sed dui luctus facilisis. Phasellus et enim a libero interdum sagittis eu eget justo. Duis suscipit tincidunt aliquam. Nulla nec gravida magna. Vestibulum laoreet diam ut tortor pretium ullamcorper. Integer sed felis quis justo imperdiet maximus eget ut mi. Pellentesque placerat facilisis nisi eu laoreet. Suspendisse commodo tempor risus, vitae feugiat enim malesuada non. Aenean lacinia odio massa, quis scelerisque lectus rhoncus volutpat. Nullam quis rhoncus dui. Donec lacinia sem porta augue tempus semper. Quisque porta egestas mauris, sed pretium nunc. Aliquam et eros porta, elementum diam ut, vulputate mauris. Pellentesque id eleifend lorem, eget blandit mi."}/>
                           <Review id={8} userID={44} userPicture={profileTemp} username={"Default"} score={8} content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rutrum blandit condimentum. Morbi sodales libero cursus, bibendum nisl at, feugiat quam. Curabitur molestie sed dui luctus facilisis. Phasellus et enim a libero interdum sagittis eu eget justo. Duis suscipit tincidunt aliquam. Nulla nec gravida magna. Vestibulum laoreet diam ut tortor pretium ullamcorper. Integer sed felis quis justo imperdiet maximus eget ut mi. Pellentesque placerat facilisis nisi eu laoreet. Suspendisse commodo tempor risus, vitae feugiat enim malesuada non. Aenean lacinia odio massa, quis scelerisque lectus rhoncus volutpat. Nullam quis rhoncus dui. Donec lacinia sem porta augue tempus semper. Quisque porta egestas mauris, sed pretium nunc. Aliquam et eros porta, elementum diam ut, vulputate mauris. Pellentesque id eleifend lorem, eget blandit mi."}/>
                       </div>
-                    </Col>
-                    <Col xl={{span: 8}} lg={{span: 8}} md={{span: 8}} sm={{span: 8}}>
-                      <Menu courses={this.vars.courses}/>
                     </Col>
                   </Row>
                 </div>
